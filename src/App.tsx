719d6e04a0c01286ffaa79194471063405ffa646
Rafaela <dev-rafaelamiranda@proton.me>
2024-02-24 16:42:06 -0300

    🎉 Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Flowmodoro from './components/Flowmodoro';
+
+function App() {
+  return (
+    <div className="App">
+      <Flowmodoro />
+    </div>
+  );
+}
+
+export default App;
